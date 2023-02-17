f51129db76ef4155566e88fb7ebc0f10084189f8
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-02-18 00:39:46 +0900

    Created component files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Download, Features, SectionWrapper } from './components';
+
 const App = () => {
   return (
     <div>
       <h1 className="text-3xl font-bold underline">React Native NFT Marketplace Showcase</h1>
+      <Download />
+      <Features />
+      <SectionWrapper />
     </div>
   );
 }
